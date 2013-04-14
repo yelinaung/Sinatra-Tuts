21ef0d75e89bca657ae3d95198042da5a62246fe
mgpyone <mgpyone@mysteryzillion.com>
2013-04-14 17:07:07 +0630

    Added Comparison between user and computer

--- game.rb
+++ game.rb
@@ -6,6 +6,7 @@ require 'sinatra'
 
 before do
     content_type :txt
+    # Define the game rule
     @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
     @throws = @defeat.keys
 end
@@ -19,5 +20,17 @@ get '/throw/:type' do
     # know they need to make a valid throw to play.
     if !@throws.include?(player_throw)
         halt 403, "You must throw one of the following: #{@throws}"
+    end
+
+    # now we can select a random throw for the computer
+    computer_throw = @throws.sample
+
+    # compare the player and computer throws to determine a winner
+    if player_throw == computer_throw
+        "You tied with the computer. Try Again!"
+    elsif computer_throw == @defeat[player_throw]
+        "Nice done; #{player_throw} beats #{computer_throw}!"
+    else
+        "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
     end
 end
